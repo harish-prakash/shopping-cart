bf38639f83811f074300b5aa38339cd9ebd2f7e7
Harish Prakash <hprak077@uottawa.ca>
2017-12-17 19:06:27 -0500

    Coupon - Percent off Nth Item of a specific type

---
+++ src/cart/shopping/generic/coupons/CouponPercentOffNthItemOfType.java
@@ -0,0 +1,86 @@
+package cart.shopping.generic.coupons;
+
+import java.util.UUID;
+
+import cart.shopping.generic.cart.Cart;
+import cart.shopping.generic.cart.CartProductWrapper;
+import cart.shopping.generic.cart.ICartItem;
+
+public class CouponPercentOffNthItemOfType extends AbstractCoupon {
+
+	// region: Instance Variables
+	private Class<?> productType;
+	private float discountPercent;
+	private int threshold;
+
+	// region: Class Initializers
+	private CouponPercentOffNthItemOfType(String title, Class<?> productType, float discountPercent, int threshold) {
+		super(title);
+		setProductType(productType);
+		setDiscountPercent(discountPercent);
+		setThreshold(threshold);
+	}
+
+	// region: Getters and Setters
+	private Class<?> getProductType() {
+		return productType;
+	}
+
+	private void setProductType(Class<?> productType) {
+		this.productType = productType;
+	}
+
+	private float getDiscountPercent() {
+		return discountPercent;
+	}
+
+	private void setDiscountPercent(float discountPercent) {
+		this.discountPercent = discountPercent;
+	}
+
+	private int getThreshold() {
+		return threshold;
+	}
+
+	private void setThreshold(int threshold) {
+		this.threshold = threshold;
+	}
+
+	// region: Overridden Methods
+	@Override
+	public UUID getCouponID() {
+
+		return UUID.fromString("a32acbc1-40e2-4b22-8993-bd2026831ed1");
+	}
+
+	@Override
+	public void applyCoupon(Cart targetCart, ICartItem cartCouponInstance) {
+
+		ICartItem cartItem = targetCart.getFirstItem();
+		int countProductTypeOccurance = 0;
+
+		while (cartItem != null) {
+
+			try {
+				if (cartItem instanceof CartProductWrapper) {
+
+					CartProductWrapper Product = (CartProductWrapper) cartItem;
+					getProductType().cast(Product.getProduct());
+
+					if (++countProductTypeOccurance >= getThreshold()) {
+
+						countProductTypeOccurance = 0;
+						float reducedCost = Product.getReducedCost();
+
+						reducedCost *= 1 - getDiscountPercent() / 100;
+						Product.setReducedCost(reducedCost);
+					}
+				}
+			} catch (java.lang.ClassCastException ex) {
+			}
+
+			cartItem = cartItem.getNextCartItem();
+		}
+	}
+
+}
